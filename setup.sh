c3e636322cbc45cb83e6c8422923e7a461a760fd
Declan Millar <declan.millar@cern.ch>
2016-05-03 12:37:23 +0200

    Updated model files.

--- setup.sh
+++
@@ -1,3 +0,0 @@
-source /afs/cern.ch/sw/lcg/external/gcc/4.8/x86_64-slc6/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.28/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
-export LD_LIBRARY_PATH=/afs/cern.ch/user/d/demillar/.RootTuple:$LD_LIBRARY_PATH
