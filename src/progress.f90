07267f3470c5ffca9176b40efa0c64e11783440f
Declan Millar <declan.millar@cern.ch>
2017-03-15 14:24:37 +0000

    print 0

--- src/progress.f90
+++ src/progress.f90
@@ -4,7 +4,7 @@ module progress
 
     implicit none
 
-    public :: progress_percentage
+    ! public :: progress_percentage
     public :: progress_bar
     integer, private :: n
     integer, private, parameter :: w = 50
@@ -13,25 +13,33 @@ contains
 
 subroutine set_total(m)
     integer, intent(in) :: m
+    integer :: i
     n = m
+
+    write(*,"(a2)", advance = "no") "["
+    do i = 0, w - 1
+        write(*,"(a1)", advance = "no") "-"
+    end do
+    write(*,"(a3, i3, a2)", advance = "no") "] (", 0, "%)"
+    write(*,"(1a1)", advance = "no") char(13)
 end subroutine set_total
 
-subroutine progress_percentage(x)
-    integer :: x, c
-    real(kind=default) :: ratio
+! subroutine progress_percentage(x)
+!     integer :: x, c
+!     real(kind=default) :: ratio
 
-    if ((x .ne. n) .and. (mod(x, (n / 100 + 1)) .ne. 0)) return
+!     if ((x .ne. n) .and. (mod(x, (n / 100 + 1)) .ne. 0)) return
 
-    ratio = x / real(n)
-    c = ratio * w
+!     ratio = x / real(n)
+!     c = ratio * w
 
-    write(*,"(1a1, a11, i3, a1)", advance = "no") char(13), "progress: ", int(ratio * 100), "%"
-    if (x == n) then
-        print*, ""
-    else 
-        write(*,"(1a1)", advance = "no") char(13)
-    end if
-end subroutine progress_percentage
+!     write(*,"(1a1, a11, i3, a1)", advance = "no") char(13), "progress: ", int(ratio * 100), "%"
+!     if (x == n) then
+!         print*, ""
+!     else 
+!         write(*,"(1a1)", advance = "no") char(13)
+!     end if
+! end subroutine progress_percentage
 
 subroutine progress_bar(x)
     integer :: x, c, i
