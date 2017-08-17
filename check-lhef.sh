caf412ab57f7b93e4d2b423b586f102bcbfc8252
declan-millar <declan.millar@cern.ch>
2017-08-17 01:13:27 +0100

    remove , in print out

--- check-lhef.sh
+++ check-lhef.sh
@@ -1,8 +1,8 @@
-for file in $(ls -1 gg-tt-bbmumuvv_SM_13TeV_CT14LL_000.lhef*)
+for file in $(ls -1 qq*.lhef)
 do
     lastline=$(tail -1 $file)
     # echo $lastline
     if [ "$lastline" != "</LesHouchesEvents>" ]; then
-        echo $file, ": fails"
+        echo $file": fails"
     fi
 done
