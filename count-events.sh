96f4b49bdf6910314df823aff4b821748ccea977
declan-millar <declan.millar@cern.ch>
2017-08-21 17:03:03 +0100

    added a script to count events in files with a wilcard specifier

---
+++ count-events.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# make newlines the only separator
+IFS=$'\n'
+
+# disable globbing
+set -f
+
+# declare count as an integer
+declare -i count
+
+for file
+do
+    count=0
+
+    for i in $(cat < $file)
+    do
+        # echo "tester: $i"
+        if [ $i == "</event>" ]
+        then
+            count+=1
+        fi
+    done
+
+    echo "$file: $count events"
+done
