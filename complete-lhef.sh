2533746b56bab16ee643b87772fb9cee304f2415
declan-millar <declan.millar@cern.ch>
2017-08-22 11:47:12 +0100

    check number of events before executing

--- complete-lhef.sh
+++ complete-lhef.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
+# make newlines the only separator
+IFS=$'\n'
+
+# disable globbing
+set -f
+
+# declare count as an integer
+declare -i count
+
 for file
 do
+
+    # count events
+    count=0
+
+    for i in $(cat < $file)
+    do
+        # echo "tester: $i"
+        if [ $i == "</event>" ]
+        then
+            count+=1
+        fi
+    done
+
+    echo "$file: $count events"
+
+    # check file has at least one event
+    if [ $count -lt 1 ]
+    then
+        echo "skipping eventless file"
+        continue
+    fi
+
     while true; do
         lastline=$(tail -1 $file | head -1)
         # echo $lastline
