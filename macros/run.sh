a8967d6c0c848f85a2a28e76bce59503ebb8d7ae
declan-millar <declan.millar@cern.ch>
2018-01-18 16:34:38 +0000

    complete partial uu files

--- macros/run.sh
+++ macros/run.sh
@@ -1,107 +1,107 @@
-./macros/generate.py -u -f 22 -Q 1nd -i 001 -t 2 -e 1362
-./macros/generate.py -u -f 22 -Q 1nd -i 008 -t 2 -e 1091
-./macros/generate.py -u -f 22 -Q 1nd -i 009 -t 2 -e 1222
-./macros/generate.py -u -f 22 -Q 1nd -i 012 -t 2 -e 1197
-./macros/generate.py -u -f 22 -Q 1nd -i 024 -t 2 -e 1389
-./macros/generate.py -u -f 22 -Q 1nd -i 026 -t 2 -e 1094
-./macros/generate.py -u -f 22 -Q 1nd -i 029 -t 2 -e 746
-./macros/generate.py -u -f 22 -Q 1nd -i 030 -t 2 -e 1335
-./macros/generate.py -u -f 22 -Q 1nd -i 031 -t 2 -e 951
-./macros/generate.py -u -f 22 -Q 1nd -i 034 -t 2 -e 551
-./macros/generate.py -u -f 22 -Q 1nd -i 035 -t 2 -e 6061
-./macros/generate.py -u -f 22 -Q 1nd -i 036 -t 2 -e 562
-./macros/generate.py -u -f 22 -Q 1nd -i 039 -t 2 -e 1142
-./macros/generate.py -u -f 22 -Q 1nd -i 054 -t 2 -e 4680
-./macros/generate.py -u -f 22 -Q 1nd -i 059 -t 2 -e 1487
-./macros/generate.py -u -f 22 -Q 1nd -i 060 -t 2 -e 1398
-./macros/generate.py -u -f 22 -Q 1nd -i 061 -t 2 -e 1204
-./macros/generate.py -u -f 22 -Q 1nd -i 067 -t 2 -e 2074
-./macros/generate.py -u -f 22 -Q 1nd -i 068 -t 2 -e 1220
-./macros/generate.py -u -f 22 -Q 1nd -i 069 -t 2 -e 1105
-./macros/generate.py -u -f 22 -Q 1nd -i 070 -t 2 -e 981
-./macros/generate.py -u -f 22 -Q 1nd -i 072 -t 2 -e 1269
-./macros/generate.py -u -f 22 -Q 1nd -i 073 -t 2 -e 1432
-./macros/generate.py -u -f 22 -Q 1nd -i 076 -t 2 -e 1168
-./macros/generate.py -u -f 22 -Q 1nd -i 077 -t 2 -e 3248
-./macros/generate.py -u -f 22 -Q 1nd -i 079 -t 2 -e 5862
-./macros/generate.py -u -f 22 -Q 1nd -i 083 -t 2 -e 2239
-./macros/generate.py -u -f 22 -Q 1nd -i 084 -t 2 -e 1378
-./macros/generate.py -u -f 22 -Q 1nd -i 087 -t 2 -e 938
-./macros/generate.py -u -f 22 -Q 1nd -i 089 -t 2 -e 1377
-./macros/generate.py -u -f 22 -Q 1nd -i 090 -t 2 -e 2141
-./macros/generate.py -u -f 22 -Q 1nd -i 091 -t 2 -e 1232
-./macros/generate.py -u -f 22 -Q 1nd -i 093 -t 2 -e 1470
-./macros/generate.py -u -f 22 -Q 1nd -i 095 -t 2 -e 1731
-./macros/generate.py -u -f 22 -Q 1nd -i 098 -t 2 -e 1256
-./macros/generate.py -u -f 22 -Q 1nd -i 104 -t 2 -e 258
-./macros/generate.py -u -f 22 -Q 1nd -i 108 -t 2 -e 1418
-./macros/generate.py -u -f 22 -Q 1nd -i 110 -t 2 -e 1412
-./macros/generate.py -u -f 22 -Q 1nd -i 111 -t 2 -e 276
-./macros/generate.py -u -f 22 -Q 1nd -i 114 -t 2 -e 1289
-./macros/generate.py -u -f 22 -Q 1nd -i 118 -t 2 -e 1466
-./macros/generate.py -u -f 22 -Q 1nd -i 119 -t 2 -e 1345
-./macros/generate.py -u -f 22 -Q 1nd -i 122 -t 2 -e 1064
-./macros/generate.py -u -f 22 -Q 1nd -i 126 -t 2 -e 346
-./macros/generate.py -u -f 22 -Q 1nd -i 134 -t 2 -e 692
-./macros/generate.py -u -f 22 -Q 1nd -i 135 -t 2 -e 1546
-./macros/generate.py -u -f 22 -Q 1nd -i 136 -t 2 -e 1051
-./macros/generate.py -u -f 22 -Q 1nd -i 137 -t 2 -e 2540
-./macros/generate.py -u -f 22 -Q 1nd -i 139 -t 2 -e 1176
-./macros/generate.py -u -f 22 -Q 1nd -i 140 -t 2 -e 568
-./macros/generate.py -u -f 22 -Q 1nd -i 149 -t 2 -e 1568
-./macros/generate.py -u -f 22 -Q 1nd -i 150 -t 2 -e 1608
-./macros/generate.py -u -f 22 -Q 1nd -i 156 -t 2 -e 860
-./macros/generate.py -u -f 22 -Q 1nd -i 157 -t 2 -e 1072
-./macros/generate.py -u -f 22 -Q 1nd -i 158 -t 2 -e 994
-./macros/generate.py -u -f 22 -Q 1nd -i 160 -t 2 -e 1193
-./macros/generate.py -u -f 22 -Q 1nd -i 161 -t 2 -e 1255
-./macros/generate.py -u -f 22 -Q 1nd -i 163 -t 2 -e 1247
-./macros/generate.py -u -f 22 -Q 1nd -i 165 -t 2 -e 1465
-./macros/generate.py -u -f 22 -Q 1nd -i 174 -t 2 -e 1192
-./macros/generate.py -u -f 22 -Q 1nd -i 175 -t 2 -e 1454
-./macros/generate.py -u -f 22 -Q 1nd -i 176 -t 2 -e 1418
-./macros/generate.py -u -f 22 -Q 1nd -i 178 -t 2 -e 1000
-./macros/generate.py -u -f 22 -Q 1nd -i 180 -t 2 -e 1216
-./macros/generate.py -u -f 22 -Q 1nd -i 181 -t 2 -e 1240
-./macros/generate.py -u -f 22 -Q 1nd -i 182 -t 2 -e 1182
-./macros/generate.py -u -f 22 -Q 1nd -i 183 -t 2 -e 1219
-./macros/generate.py -u -f 22 -Q 1nd -i 185 -t 2 -e 1433
-./macros/generate.py -u -f 22 -Q 1nd -i 189 -t 2 -e 2426
-./macros/generate.py -u -f 22 -Q 1nd -i 194 -t 2 -e 1316
-./macros/generate.py -u -f 22 -Q 1nd -i 195 -t 2 -e 1525
-./macros/generate.py -u -f 22 -Q 1nd -i 198 -t 2 -e 36
-./macros/generate.py -u -f 22 -Q 1nd -i 199 -t 2 -e 187
-./macros/generate.py -u -f 22 -Q 1nd -i 204 -t 2 -e 1031
-./macros/generate.py -u -f 22 -Q 1nd -i 205 -t 2 -e 1468
-./macros/generate.py -u -f 22 -Q 1nd -i 209 -t 2 -e 1353
-./macros/generate.py -u -f 22 -Q 1nd -i 211 -t 2 -e 1069
-./macros/generate.py -u -f 22 -Q 1nd -i 213 -t 2 -e 1451
-./macros/generate.py -u -f 22 -Q 1nd -i 214 -t 2 -e 1310
-./macros/generate.py -u -f 22 -Q 1nd -i 218 -t 2 -e 1107
-./macros/generate.py -u -f 22 -Q 1nd -i 219 -t 2 -e 1359
-./macros/generate.py -u -f 22 -Q 1nd -i 230 -t 2 -e 1137
-./macros/generate.py -u -f 22 -Q 1nd -i 231 -t 2 -e 1252
-./macros/generate.py -u -f 22 -Q 1nd -i 242 -t 2 -e 1296
-./macros/generate.py -u -f 22 -Q 1nd -i 244 -t 2 -e 2286
-./macros/generate.py -u -f 22 -Q 1nd -i 246 -t 2 -e 1337
-./macros/generate.py -u -f 22 -Q 1nd -i 247 -t 2 -e 1281
-./macros/generate.py -u -f 22 -Q 1nd -i 248 -t 2 -e 1309
-./macros/generate.py -u -f 22 -Q 1nd -i 250 -t 2 -e 762
-./macros/generate.py -u -f 22 -Q 1nd -i 254 -t 2 -e 1368
-./macros/generate.py -u -f 22 -Q 1nd -i 256 -t 2 -e 1070
-./macros/generate.py -u -f 22 -Q 1nd -i 258 -t 2 -e 1491
-./macros/generate.py -u -f 22 -Q 1nd -i 262 -t 2 -e 863
-./macros/generate.py -u -f 22 -Q 1nd -i 265 -t 2 -e 54
-./macros/generate.py -u -f 22 -Q 1nd -i 266 -t 2 -e 772
-./macros/generate.py -u -f 22 -Q 1nd -i 269 -t 2 -e 2387
-./macros/generate.py -u -f 22 -Q 1nd -i 275 -t 2 -e 1136
-./macros/generate.py -u -f 22 -Q 1nd -i 280 -t 2 -e 1158
-./macros/generate.py -u -f 22 -Q 1nd -i 281 -t 2 -e 1524
-./macros/generate.py -u -f 22 -Q 1nd -i 282 -t 2 -e 1247
-./macros/generate.py -u -f 22 -Q 1nd -i 286 -t 2 -e 1368
-./macros/generate.py -u -f 22 -Q 1nd -i 287 -t 2 -e 1210
-./macros/generate.py -u -f 22 -Q 1nd -i 289 -t 2 -e 776
-./macros/generate.py -u -f 22 -Q 1nd -i 290 -t 2 -e 1342
-./macros/generate.py -u -f 22 -Q 1nd -i 291 -t 2 -e 1046
-./macros/generate.py -u -f 22 -Q 1nd -i 293 -t 2 -e 1499
-./macros/generate.py -u -f 22 -Q 1nd -i 296 -t 2 -e 692
+./macros/generate.py -u -f 22 -Q 1nd -i 0001 -e 1362
+./macros/generate.py -u -f 22 -Q 1nd -i 0008 -e 1091
+./macros/generate.py -u -f 22 -Q 1nd -i 0009 -e 1222
+./macros/generate.py -u -f 22 -Q 1nd -i 0012 -e 1197
+./macros/generate.py -u -f 22 -Q 1nd -i 0024 -e 1389
+./macros/generate.py -u -f 22 -Q 1nd -i 0026 -e 1094
+./macros/generate.py -u -f 22 -Q 1nd -i 0029 -e 746
+./macros/generate.py -u -f 22 -Q 1nd -i 0030 -e 1335
+./macros/generate.py -u -f 22 -Q 1nd -i 0031 -e 951
+./macros/generate.py -u -f 22 -Q 1nd -i 0034 -e 551
+./macros/generate.py -u -f 22 -Q 1nd -i 0035 -e 6061
+./macros/generate.py -u -f 22 -Q 1nd -i 0036 -e 562
+./macros/generate.py -u -f 22 -Q 1nd -i 0039 -e 1142
+./macros/generate.py -u -f 22 -Q 1nd -i 0054 -e 4680
+./macros/generate.py -u -f 22 -Q 1nd -i 0059 -e 1487
+./macros/generate.py -u -f 22 -Q 1nd -i 0060 -e 1398
+./macros/generate.py -u -f 22 -Q 1nd -i 0061 -e 1204
+./macros/generate.py -u -f 22 -Q 1nd -i 0067 -e 2074
+./macros/generate.py -u -f 22 -Q 1nd -i 0068 -e 1220
+./macros/generate.py -u -f 22 -Q 1nd -i 0069 -e 1105
+./macros/generate.py -u -f 22 -Q 1nd -i 0070 -e 981
+./macros/generate.py -u -f 22 -Q 1nd -i 0072 -e 1269
+./macros/generate.py -u -f 22 -Q 1nd -i 0073 -e 1432
+./macros/generate.py -u -f 22 -Q 1nd -i 0076 -e 1168
+./macros/generate.py -u -f 22 -Q 1nd -i 0077 -e 3248
+./macros/generate.py -u -f 22 -Q 1nd -i 0079 -e 5862
+./macros/generate.py -u -f 22 -Q 1nd -i 0083 -e 2239
+./macros/generate.py -u -f 22 -Q 1nd -i 0084 -e 1378
+./macros/generate.py -u -f 22 -Q 1nd -i 0087 -e 938
+./macros/generate.py -u -f 22 -Q 1nd -i 0089 -e 1377
+./macros/generate.py -u -f 22 -Q 1nd -i 0090 -e 2141
+./macros/generate.py -u -f 22 -Q 1nd -i 0091 -e 1232
+./macros/generate.py -u -f 22 -Q 1nd -i 0093 -e 1470
+./macros/generate.py -u -f 22 -Q 1nd -i 0095 -e 1731
+./macros/generate.py -u -f 22 -Q 1nd -i 0098 -e 1256
+./macros/generate.py -u -f 22 -Q 1nd -i 0104 -e 258
+./macros/generate.py -u -f 22 -Q 1nd -i 0108 -e 1418
+./macros/generate.py -u -f 22 -Q 1nd -i 0110 -e 1412
+./macros/generate.py -u -f 22 -Q 1nd -i 0111 -e 276
+./macros/generate.py -u -f 22 -Q 1nd -i 0114 -e 1289
+./macros/generate.py -u -f 22 -Q 1nd -i 0118 -e 1466
+./macros/generate.py -u -f 22 -Q 1nd -i 0119 -e 1345
+./macros/generate.py -u -f 22 -Q 1nd -i 0122 -e 1064
+./macros/generate.py -u -f 22 -Q 1nd -i 0126 -e 346
+./macros/generate.py -u -f 22 -Q 1nd -i 0134 -e 692
+./macros/generate.py -u -f 22 -Q 1nd -i 0135 -e 1546
+./macros/generate.py -u -f 22 -Q 1nd -i 0136 -e 1051
+./macros/generate.py -u -f 22 -Q 1nd -i 0137 -e 2540
+./macros/generate.py -u -f 22 -Q 1nd -i 0139 -e 1176
+./macros/generate.py -u -f 22 -Q 1nd -i 0140 -e 568
+./macros/generate.py -u -f 22 -Q 1nd -i 0149 -e 1568
+./macros/generate.py -u -f 22 -Q 1nd -i 0150 -e 1608
+./macros/generate.py -u -f 22 -Q 1nd -i 0156 -e 860
+./macros/generate.py -u -f 22 -Q 1nd -i 0157 -e 1072
+./macros/generate.py -u -f 22 -Q 1nd -i 0158 -e 994
+./macros/generate.py -u -f 22 -Q 1nd -i 0160 -e 1193
+./macros/generate.py -u -f 22 -Q 1nd -i 0161 -e 1255
+./macros/generate.py -u -f 22 -Q 1nd -i 0163 -e 1247
+./macros/generate.py -u -f 22 -Q 1nd -i 0165 -e 1465
+./macros/generate.py -u -f 22 -Q 1nd -i 0174 -e 1192
+./macros/generate.py -u -f 22 -Q 1nd -i 0175 -e 1454
+./macros/generate.py -u -f 22 -Q 1nd -i 0176 -e 1418
+./macros/generate.py -u -f 22 -Q 1nd -i 0178 -e 1000
+./macros/generate.py -u -f 22 -Q 1nd -i 0180 -e 1216
+./macros/generate.py -u -f 22 -Q 1nd -i 0181 -e 1240
+./macros/generate.py -u -f 22 -Q 1nd -i 0182 -e 1182
+./macros/generate.py -u -f 22 -Q 1nd -i 0183 -e 1219
+./macros/generate.py -u -f 22 -Q 1nd -i 0185 -e 1433
+./macros/generate.py -u -f 22 -Q 1nd -i 0189 -e 2426
+./macros/generate.py -u -f 22 -Q 1nd -i 0194 -e 1316
+./macros/generate.py -u -f 22 -Q 1nd -i 0195 -e 1525
+./macros/generate.py -u -f 22 -Q 1nd -i 0198 -e 36
+./macros/generate.py -u -f 22 -Q 1nd -i 0199 -e 187
+./macros/generate.py -u -f 22 -Q 1nd -i 0204 -e 1031
+./macros/generate.py -u -f 22 -Q 1nd -i 0205 -e 1468
+./macros/generate.py -u -f 22 -Q 1nd -i 0209 -e 1353
+./macros/generate.py -u -f 22 -Q 1nd -i 0211 -e 1069
+./macros/generate.py -u -f 22 -Q 1nd -i 0213 -e 1451
+./macros/generate.py -u -f 22 -Q 1nd -i 0214 -e 1310
+./macros/generate.py -u -f 22 -Q 1nd -i 0218 -e 1107
+./macros/generate.py -u -f 22 -Q 1nd -i 0219 -e 1359
+./macros/generate.py -u -f 22 -Q 1nd -i 0230 -e 1137
+./macros/generate.py -u -f 22 -Q 1nd -i 0231 -e 1252
+./macros/generate.py -u -f 22 -Q 1nd -i 0242 -e 1296
+./macros/generate.py -u -f 22 -Q 1nd -i 0244 -e 2286
+./macros/generate.py -u -f 22 -Q 1nd -i 0246 -e 1337
+./macros/generate.py -u -f 22 -Q 1nd -i 0247 -e 1281
+./macros/generate.py -u -f 22 -Q 1nd -i 0248 -e 1309
+./macros/generate.py -u -f 22 -Q 1nd -i 0250 -e 762
+./macros/generate.py -u -f 22 -Q 1nd -i 0254 -e 1368
+./macros/generate.py -u -f 22 -Q 1nd -i 0256 -e 1070
+./macros/generate.py -u -f 22 -Q 1nd -i 0258 -e 1491
+./macros/generate.py -u -f 22 -Q 1nd -i 0262 -e 863
+./macros/generate.py -u -f 22 -Q 1nd -i 0265 -e 54
+./macros/generate.py -u -f 22 -Q 1nd -i 0266 -e 772
+./macros/generate.py -u -f 22 -Q 1nd -i 0269 -e 2387
+./macros/generate.py -u -f 22 -Q 1nd -i 0275 -e 1136
+./macros/generate.py -u -f 22 -Q 1nd -i 0280 -e 1158
+./macros/generate.py -u -f 22 -Q 1nd -i 0281 -e 1524
+./macros/generate.py -u -f 22 -Q 1nd -i 0282 -e 1247
+./macros/generate.py -u -f 22 -Q 1nd -i 0286 -e 1368
+./macros/generate.py -u -f 22 -Q 1nd -i 0287 -e 1210
+./macros/generate.py -u -f 22 -Q 1nd -i 0289 -e 776
+./macros/generate.py -u -f 22 -Q 1nd -i 0290 -e 1342
+./macros/generate.py -u -f 22 -Q 1nd -i 0291 -e 1046
+./macros/generate.py -u -f 22 -Q 1nd -i 0293 -e 1499
+./macros/generate.py -u -f 22 -Q 1nd -i 0296 -e 692
