cf76e9224f998f24c51c05da1b623f1a83e3a8eb
Declan Millar <declan.millar@ibm.com>
2022-05-09 01:07:31 +0100

    Format python file with black

--- generate.py
+++ generate.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Python run script for the event generator Fortran executable.
+Python script to run the event generator executable.
 Generates a config file and runs the executable using it.
 Runs locally or submits a job to the lxplus/iridis/qmul batch system.
 """
-
-__author__ = "Declan Millar"
-__version__ = "dev"
-__license__ = "MIT"
 
 import os
 import argparse
@@ -24,13 +20,10 @@ def check_config(config) -> None:
 
     # Check executable exists
     if not os.path.isfile(config["filename"]):
-        raise FileNotFoundError(
-            f"Executable {config['filename']} does not exist")
+        raise FileNotFoundError(f"Executable {config['filename']} does not exist")
 
     if not os.path.isdir(config["data_dir"]):
-        print(
-            f"Warning: {config['data_dir']} does not exist; it will be created."
-        )
+        print(f"Warning: {config['data_dir']} does not exist; it will be created.")
         os.makedirs(config["data_dir"])
 
     # Check model_name file exists
@@ -39,8 +32,9 @@ def check_config(config) -> None:
         raise FileNotFoundError(f"{model_path} does not exist.")
 
     # Drell-Yan has no gluon-mediated channels
-    if config["final_state"] == -1 and (config["initial_state"] == 1
-                                        or config["initial_state"] == 2):
+    if config["final_state"] == -1 and (
+        config["initial_state"] == 1 or config["initial_state"] == 2
+    ):
         raise ValueError("Drell-Yan has no gluon-mediated channels.")
 
 
@@ -115,49 +109,49 @@ def create_output_names(config):
 
     process = process + final_state_names[config["final_state"]]
 
-    pdf_name = PDF(config['pdf']).name
+    pdf_name = PDF(config["pdf"]).name
 
     # Include non-default major tag in output file name
     tag = ""
-    if config['ppbar'] == 1:
+    if config["ppbar"] == 1:
         tag += "_ppbar"
-    if config['interference'] != 1:
+    if config["interference"] != 1:
         tag += f"_int{config['interference']}"
-    if config['nwa']:
+    if config["nwa"]:
         tag += "_nwa"
-    if config['cut']:
+    if config["cut"]:
         tag += "_cut"
-    if config['rambo']:
+    if config["rambo"]:
         tag += "_rambo"
-    if config['background'] is False and config['flatten_integrand'] is False:
+    if config["background"] is False and config["flatten_integrand"] is False:
         tag += "_unmapped"
-    if config['ecm_low'] is not None or config['ecm_up'] is not None:
+    if config["ecm_low"] is not None or config["ecm_up"] is not None:
         tag += f"_{config['ecm_low']}-{config['ecm_up']}"
-    if config['tag'] is not None:
-        tag += "_" + config['tag']
+    if config["tag"] is not None:
+        tag += "_" + config["tag"]
 
     events_name = f"{process}_{config['model_name']}_{str(config['sqrts'])}TeV_{pdf_name}{tag}"
     grid_name = f"{grid_proc}_{config['model_name']}_{str(config['sqrts'])}TeV_{pdf_name}{tag}"
 
     # Get new file index
     datafiles = [
-        f for f in os.listdir(config['data_dir'])
-        if os.path.isfile(os.path.join(config['data_dir'], f))
+        f
+        for f in os.listdir(config["data_dir"])
+        if os.path.isfile(os.path.join(config["data_dir"], f))
     ]
     filtered = fnmatch.filter(datafiles, events_name + "_??.lhef")
-    new_index = int(
-        config['index']) if config['index'] is not None else len(filtered) + 1
+    new_index = int(config["index"]) if config["index"] is not None else len(filtered) + 1
     events_name = events_name + f"_{new_index:03d}"
 
-    events_path = config['data_dir'] + events_name
-    grid_path = config['data_dir'] + grid_name
+    events_path = config["data_dir"] + events_name
+    grid_path = config["data_dir"] + grid_name
 
-    wgt = "" if config['unweighted'] else "_wgt"
+    wgt = "" if config["unweighted"] else "_wgt"
 
     grid_name = f"{grid_path}.grid"
     xsec_file = f"{grid_path}.txt"
 
-    new_grid = not os.path.isfile(grid_name) or config['overwrite']
+    new_grid = not os.path.isfile(grid_name) or config["overwrite"]
 
     events_path = events_path + wgt
     lhef = events_path + ".lhef"
@@ -190,8 +184,7 @@ def main():
 
     print("checked config:\n", config)
 
-    config_name, handler, new_grid, grid_name, xsec_file, log, lhef = create_output_names(
-        config)
+    config_name, handler, new_grid, grid_name, xsec_file, log, lhef = create_output_names(config)
 
     config_string = (
         f"{new_grid} ! new_grid\n"
@@ -225,7 +218,8 @@ def main():
         f"{0 if config['ecm_low'] is None else config['ecm_low']}.d3 ! ecm_low\n"
         f"{0 if config['ecm_up'] is None else config['ecm_up']}.d3 ! ecm_up\n"
         f"{config['job']} ! batch\n"
-        f"{config['cut']} ! cut\n")
+        f"{config['cut']} ! cut\n"
+    )
 
     with open(config_name, "w") as config_file:
         config_file.write(config_string)
@@ -245,7 +239,8 @@ def main():
             #    f"cd {run_directory}\n"
             #    f"{run_directory}{executable} < {config_name} > {log}\n"
             f"{args.filename} < {config_name} > {log}\n"
-            f"gzip -v9 {lhef} >> {log}\n")
+            f"gzip -v9 {lhef} >> {log}\n"
+        )
 
         with open(handler, "w") as handler_file:
             handler_file.write(handler)
@@ -253,10 +248,9 @@ def main():
         print(f"handler: {handler}")
 
         subprocess.call(f"chmod a+x {handler}", shell=True)
-        subprocess.call(f"qsub -l walltime={args.walltime} {handler}",
-                        shell=True)
+        subprocess.call(f"qsub -l walltime={args.walltime} {handler}", shell=True)
     else:
-        command = (f"{args.filename} < {config_name} | tee {log}")
+        command = f"{args.filename} < {config_name} | tee {log}"
         # f" && gzip -v9 {lhef} >> {log}")
         subprocess.call(command, shell=True)
 
@@ -276,103 +270,74 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Generate X-tt events.")
 
     # File and directory options
-    parser.add_argument("-f", "--filename", help="Executable file name.",
-                        default="./bin/generator")
-    parser.add_argument("-d", "--data_dir", help="Executable file name.",
-                        default="./data/")
+    parser.add_argument("-f", "--filename", help="Executable file name.", default="./bin/generator")
+    parser.add_argument("-d", "--data_dir", help="Executable file name.", default="./data/")
 
     # Job options
-    parser.add_argument("-w", "--walltime",
-                        help="Set Iridis walltime in 'hh:mm:ss' format",
-                        default="60:00:00")
-    parser.add_argument("-j", "--job", help="Submit as a batch job.",
-                        action="store_true")
-    parser.add_argument("-o", "--overwrite",
-                        help="Overwrite existing grid file.",
-                        action="store_true")
-    parser.add_argument("-v", "--verbose", help="Print extra run information.",
-                        action="store_true")
-    parser.add_argument("--tag", help="Add a name tag to output files.",
-                        default=None)
-    parser.add_argument("-i", "--index", help="Overwrite filename index tag.",
-                        default=None)
+    parser.add_argument(
+        "-w", "--walltime", help="Set Iridis walltime in 'hh:mm:ss' format", default="60:00:00"
+    )
+    parser.add_argument("-j", "--job", help="Submit as a batch job.", action="store_true")
+    parser.add_argument(
+        "-o", "--overwrite", help="Overwrite existing grid file.", action="store_true"
+    )
+    parser.add_argument("-v", "--verbose", help="Print extra run information.", action="store_true")
+    parser.add_argument("--tag", help="Add a name tag to output files.", default=None)
+    parser.add_argument("-i", "--index", help="Overwrite filename index tag.", default=None)
     # parser.add_argument("-q",
     #                     "--queue",
     #                     help="lxbatch queue",
     #                     default="1nw") # No access to lxplus
 
     # Collider options
-    parser.add_argument("-s", "--sqrts", help="Set collider energy (s hat).",
-                        type=int, default=13)
-    parser.add_argument("--ecm_low",
-                        help="Set a lower limit on the collider energy.",
-                        type=int, default=None)
-    parser.add_argument("--ecm_up",
-                        help="Set an upper limit on the collider energy",
-                        type=int, default=None)
-    parser.add_argument("--ppbar",
-                        help="Set initial collider state. 0: pp, 1: pp~",
-                        default=0)
+    parser.add_argument("-s", "--sqrts", help="Set collider energy (s hat).", type=int, default=13)
+    parser.add_argument(
+        "--ecm_low", help="Set a lower limit on the collider energy.", type=int, default=None
+    )
+    parser.add_argument(
+        "--ecm_up", help="Set an upper limit on the collider energy", type=int, default=None
+    )
+    parser.add_argument("--ppbar", help="Set initial collider state. 0: pp, 1: pp~", default=0)
 
     # Physics model
-    parser.add_argument("-m", "--model_name",
-                        help="Set the physics model name.", default="SM")
+    parser.add_argument("-m", "--model_name", help="Set the physics model name.", default="SM")
 
     # Final/initial state
-    parser.add_argument("-F", "--final_state", help="Set final state ID.",
-                        type=int, default=1)
-    parser.add_argument("-I", "--initial_state", help="Set ititial state ID.",
-                        type=int, default=1)
-    parser.add_argument("-G", "--gg",
-                        help="Include gluon-gluon initial state.",
-                        action="store_true")
-    parser.add_argument("-Q", "--qq",
-                        help="Include quark-quark initial state.",
-                        action="store_true")
-    parser.add_argument("-U", "--uu", help="Include up-up initial state.",
-                        action="store_true")
-    parser.add_argument("-D", "--dd", help="Include down-down initial state.",
-                        action="store_true")
+    parser.add_argument("-F", "--final_state", help="Set final state ID.", type=int, default=1)
+    parser.add_argument("-I", "--initial_state", help="Set ititial state ID.", type=int, default=1)
+    parser.add_argument(
+        "-G", "--gg", help="Include gluon-gluon initial state.", action="store_true"
+    )
+    parser.add_argument(
+        "-Q", "--qq", help="Include quark-quark initial state.", action="store_true"
+    )
+    parser.add_argument("-U", "--uu", help="Include up-up initial state.", action="store_true")
+    parser.add_argument("-D", "--dd", help="Include down-down initial state.", action="store_true")
 
     # Mediators
-    parser.add_argument("-A", help="Include photon mediated interaction.",
-                        action="store_false")
-    parser.add_argument("-Z", help="Include Z boson mediated interaction.",
-                        action="store_false")
-    parser.add_argument("-X", help="Include Z' boson mediated interactions.",
-                        action="store_false")
+    parser.add_argument("-A", help="Include photon mediated interaction.", action="store_false")
+    parser.add_argument("-Z", help="Include Z boson mediated interaction.", action="store_false")
+    parser.add_argument("-X", help="Include Z' boson mediated interactions.", action="store_false")
 
     # QFT interference
-    parser.add_argument("--interference", help="Set interference", type=int,
-                        default=1)
+    parser.add_argument("--interference", help="Set interference", type=int, default=1)
 
     # 2to6 signal and background
-    parser.add_argument("--signal", help="Include ttbar 2to6 signal.",
-                        action="store_false")
-    parser.add_argument("--background", help="Include ttbar 2to6 background.",
-                        action="store_true")
+    parser.add_argument("--signal", help="Include ttbar 2to6 signal.", action="store_false")
+    parser.add_argument("--background", help="Include ttbar 2to6 background.", action="store_true")
 
     # Parton Distribution Functions
-    parser.add_argument("--pdf", help="Parton Density Function ID.", type=int,
-                        default=11)
+    parser.add_argument("--pdf", help="Parton Density Function ID.", type=int, default=11)
 
     # Integration options
-    parser.add_argument("-N", "--itmx", help="Number of VAMP iterations.",
-                        type=int, default=16)
-    parser.add_argument("-n", "--ncall", help="Number of VAMP calls.",
-                        type=int, default=16000000)
-    parser.add_argument("-e", "--nevents", help="Number of unweighted events.",
-                        default=10000)
-    parser.add_argument("--flatten_integrand", help="Flatten integrand.",
-                        action="store_false")
-    parser.add_argument("--unweighted", help="Generate unweighted events.",
-                        action="store_false")
-    parser.add_argument("--rambo", help="Use RAMBO for phase space.",
-                        action="store_true")
-    parser.add_argument("--nwa", help="Use the Narrow Width Approximation.",
-                        action="store_true")
-    parser.add_argument("--cut", help="Apply detector cuts.",
-                        action="store_true")
+    parser.add_argument("-N", "--itmx", help="Number of VAMP iterations.", type=int, default=16)
+    parser.add_argument("-n", "--ncall", help="Number of VAMP calls.", type=int, default=16000000)
+    parser.add_argument("-e", "--nevents", help="Number of unweighted events.", default=10000)
+    parser.add_argument("--flatten_integrand", help="Flatten integrand.", action="store_false")
+    parser.add_argument("--unweighted", help="Generate unweighted events.", action="store_false")
+    parser.add_argument("--rambo", help="Use RAMBO for phase space.", action="store_true")
+    parser.add_argument("--nwa", help="Use the Narrow Width Approximation.", action="store_true")
+    parser.add_argument("--cut", help="Apply detector cuts.", action="store_true")
 
     return parser.parse_args()
 
